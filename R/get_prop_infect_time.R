7c4e544905b6d433710092d56eb392d798e4db5c
Justin Lessler <justin@jhu.edu>
2020-06-30 01:16:19 -0400

    reparameterized so offset makes more sense. Updae doc to

--- R/get_prop_infect_time.R
+++ R/get_prop_infect_time.R
@@ -5,7 +5,7 @@
 #' Infectious period is assumed to be gamma distributed.
 #'
 #' @param delay Numeric greater than 0. Time from symptom onset to detection
-#' @param offset Numeric. Offset. Default is 2.31
+#' @param offset Numeric. Offset of infectiousnes compared to symptons onset Default is -2.31
 #' @param shape Numeric. Shape of the gamma distribution of infectious period.
 #'    Default is 3.
 #' @param rate Numeric. Rate of the gamma distribution of infectious period.
@@ -14,7 +14,8 @@
 #' @return Numeric. Proportion of infectious period reduced given the
 #'   distribution of infectiousness and time from symptom onset to detection.
 #' @export
-get_prop_infect_time <- function(delay, offset = 2.31,
+get_prop_infect_time <- function(delay, offset = -2.31,
                                  shape = 3, rate = 0.69) {
-  stats::pgamma(delay + offset, shape = shape, rate = rate)
+  if (delay<offset) {return (0)}
+  stats::pgamma(delay - offset, shape = shape, rate = rate)
 }
