c8f786dd0d08dc55cffd13e49c98e6c094d5b298
Lucy McGowan <lucydagostino@gmail.com>
2020-09-26 11:36:28 -0400

    simplify fx

--- R/get_prop_undetected.R
+++ R/get_prop_undetected.R
@@ -1,8 +1,7 @@
 get_prop_undetected <- function(quarantine_days) {
   d <- boot_lnorm_params_covid
-  d <- d[rep(seq_len(nrow(d)), each = quarantine_days), ]
-  d$d <- rep(1:quarantine_days, length.out = nrow(d))
+  d$d <- quarantine_days
   d$p <- stats::plnorm(d$d, meanlog = d$meanlog, sdlog = d$sdlog,
                 lower.tail = FALSE)
-  stats::median(d$p[d$d == quarantine_days])
+  stats::median(d$p)
 }
