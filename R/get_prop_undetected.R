a89063299864225f702766522c9f67086c28c1bb
Lucy McGowan <lucydagostino@gmail.com>
2020-09-29 13:15:16 -0400

    update values

--- R/get_prop_undetected.R
+++ R/get_prop_undetected.R
@@ -1,7 +1,5 @@
 get_prop_undetected <- function(quarantine_days) {
-  d <- boot_lnorm_params_covid
-  d$d <- quarantine_days
-  d$p <- stats::plnorm(d$d, meanlog = d$meanlog, sdlog = d$sdlog,
+  stats::plnorm(quarantine_days, meanlog = 1.63, sdlog = 0.5,
                 lower.tail = FALSE)
-  stats::median(d$p)
+  ## based on BMJ Open meta analysis: http://dx.doi.org/10.1136/bmjopen-2020-039652
 }
